970f3d5a9bb4fcf3ff434e9b500e04ece710a0b2
Student8 <Student8@10.8.33.161>
2017-05-05 13:23:14 -0400

    update 1

---
+++ GroupSymposium/src/rhbattlesandstuff/Unit.java
@@ -0,0 +1,15 @@
+package rhbattlesandstuff;
+
+public class Unit {
+
+	private String[] moves;
+	private int[] stats;
+	private int dmgTaken;
+	private int dmgDealt;
+	private String[] names;
+	
+	public Unit() {
+		
+	}
+
+}
