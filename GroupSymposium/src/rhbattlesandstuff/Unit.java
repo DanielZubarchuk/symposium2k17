01096c80e8aad0a763930cfa9d6ab0ac08349554
Student8 <Student8@10.8.33.161>
2017-05-08 13:59:34 -0400

    5/8/17 updated with some methods - methods don't work as of yet

--- GroupSymposium/src/rhbattlesandstuff/Unit.java
+++ GroupSymposium/src/rhbattlesandstuff/Unit.java
@@ -11,5 +11,52 @@ public class Unit {
 	public Unit() {
 		
 	}
+	
+	public void battle(Player a, Enemy b){
+		while (a[0] > 0 || b[0] > 0){
+			if (a[3] > b[3]){
+				dealDamage(chooseMove(a),a,b);
+				if (b[0] > 0){
+					dealDamage(chooseMove(b),b,a);
+				}
+			}
+			else if (b[3] > a[3]){
+				dealDamage(chooseMove(b),b,a);
+				if (a[0] > 0){
+					dealDamage(chooseMove(a),a,b);
+				}
+			}	
+		}
+		if (a[0] != 0){
+			outcome();
+		}
+		else {
+			gameOver();
+		}
+	}
+	
+	private void gameOver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void outcome() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void dealDamage(String string, Player a, Enemy b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String chooseMove(Player n){
+		return null;
+		
+	}
+	
+	public String chooseMove(Enemy n){
+		for (int i = 4)
+	}
 
 }
