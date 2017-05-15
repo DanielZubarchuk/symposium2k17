7a5bded3646142fe2d7b9067f19185e9edcd067d
Student8 <Student8@10.8.33.167>
2017-05-15 13:29:24 -0400

    ?

---
+++ GroupSymposium/src/joeyDaniel/BossWall.java
@@ -0,0 +1,17 @@
+package joeyDaniel;
+
+import guiPractice.components.Graphic;
+
+public class BossWall extends Graphic implements Obstruction {
+
+	public BossWall(int x, int y) {
+		super(x, y, 1, "src/resources/nothing.png");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BossWall() {
+		super(0, 0, 1, "src/resources/nothing.png");
+		// TODO Auto-generated constructor stub
+	}
+
+}
