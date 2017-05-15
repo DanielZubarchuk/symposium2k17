98b1a17e5cc78d3aed452989b53d05677119c3d5
Student8 <Student8@10.8.33.167>
2017-05-15 13:04:00 -0400

    Wood Walls

---
+++ GroupSymposium/src/joeyDaniel/WoodWall.java
@@ -0,0 +1,13 @@
+package joeyDaniel;
+
+import guiPractice.components.Graphic;
+
+public class WoodWall extends Graphic implements Obstruction {
+
+	public WoodWall(int x, int y) {
+		super(x, y,0.1953125, "src/resources/wood.jpg");
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
