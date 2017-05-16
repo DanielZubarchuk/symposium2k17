63e78d65aa803a83080f67553b8a0c2ffbb6bb3b
Student8 <Student8@10.8.33.167>
2017-05-16 13:58:43 -0400

    Fixed error, can display one 7x7

---
+++ GroupSymposium/src/joeyDaniel/Blank.java
@@ -0,0 +1,12 @@
+package joeyDaniel;
+
+import guiPractice.components.Graphic;
+
+public class Blank extends Graphic {
+
+	public Blank() {
+		super(0,0,"");
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- GroupSymposium/src/joeyDaniel/Floor1.java
+++ GroupSymposium/src/joeyDaniel/Floor1.java
@@ -13,6 +13,56 @@ public class Floor1 extends Screen{
 
 	public Floor1(int width, int height) {
 		super(width, height);
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		initWalls();
+		background();
+		playerCoordinate = new int[2];
+		playerCoordinate[0] = 8;
+		playerCoordinate[1] = 8;
+//		for(Graphic[] x:layout){
+//			for(Graphic g:x){
+//				if(g == null){
+//					System.out.print("Null");
+//				}else if(g instanceof Obstruction){
+//					System.out.print("Wall");
+//				}else{
+//					System.out.print("idk");
+//				}
+//			}
+//			System.out.println("");
+//		}
+		UpdateScreen(viewObjects);
+	}
+	
+	private void UpdateScreen(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		int y = 0;
+		for(int row = playerCoordinate[0]-3; row<=playerCoordinate[0]+3;row++ ){
+			for(int col = playerCoordinate[1]-3; col<=playerCoordinate[1]+3;col++ ){
+				if(layout[row][col] instanceof Obstruction){
+					layout[row][col] = new WoodWall(x*100,y*100);
+					viewObjects.add(layout[row][col]);
+				}
+				x++;
+			}
+			y++;
+			x=0;
+		}
+	}
+
+	public void background(){
+		for(int i = 0; i < 7;i++){
+			for(int x = 0; x < 7;x++){
+				addObject(new GrassPanel(i*100,x*100));
+			}
+		}
+	}
+	
+	public void initWalls(){
 		layout = new Graphic[18][18];
 		for(int i = 0; i<layout.length; i++){
 			for(int j = 0; j<layout[i].length; j++){
@@ -36,35 +86,12 @@ public class Floor1 extends Screen{
 				layout[row][13] = new WoodWall();
 			}
 		}
-	}
-
-	@Override
-	public void initObjects(ArrayList<Visible> viewObjects) {
-		// TODO Auto-generated method stub
-		background();
-		playerCoordinate = new int[2];
-		playerCoordinate[0] = 3;
-		playerCoordinate[1] = 3;
-		UpdateScreen(viewObjects);
-	}
-	
-	private void UpdateScreen(ArrayList<Visible> viewObjects) {
-		// TODO Auto-generated method stub
-		
-		for(int row = playerCoordinate[0]-3; row<=playerCoordinate[0]+3;row++ ){
-			for(int col = playerCoordinate[1]-3; col<=playerCoordinate[1]+3;col++ ){
-				if(layout[row][col] != null){
-					viewObjects.add(layout[row][col]);
+		for(int row = 3; row < layout.length-3; row++){
+			for(int col = 3; col<layout[row].length-3;col++){
+				if(layout[row][col] == null){
+					layout[row][col] = new Blank();
 				}
-			}
-		}
-	}
-
-	public void background(){
-		for(int i = 0; i < 7;i++){
-			for(int x = 0; x < 7;x++){
-				addObject(new GrassPanel(i*100,x*100));
-			}
+			}				
 		}
 	}
 	
