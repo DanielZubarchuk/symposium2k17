b381d6c852538c44217c54819be4bbc71326c592
Student8 <Student8@10.8.33.167>
2017-05-17 13:54:25 -0400

    KeyPressed ???? Needs work

--- GroupSymposium/src/joeyDaniel/Floor.java
+++ GroupSymposium/src/joeyDaniel/Floor.java
@@ -8,31 +8,56 @@ import guiPractice.Screen;
 import guiPractice.components.Visible;
 
 public abstract class Floor extends Screen implements KeyListener{
+	//	left = 37
+	//	up = 38
+	//	right = 39
+	//	down = 40
 
-	
-	
 	public Floor() {
 		super(716,738);
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent b) {
 		// TODO Auto-generated method stub
-		System.out.println("Key Pressed");
+		if(b.getKeyCode() == 38){
+//			System.out.println("Up");
+			if(Map.x.getLocation()[0]-->=3){
+				Map.x.setLocation(Map.x.getLocation()[0]--, Map.x.getLocation()[1]);
+			}
+			
+			
+			
+		}else if(b.getKeyCode() == 40){
+//			System.out.println("Down");
+			if(Map.x.getLocation()[0]++>=3){
+				Map.x.setLocation(Map.x.getLocation()[0]--, Map.x.getLocation()[1]);
+			}
+			
+			
+		}else if(b.getKeyCode() == 37){
+//			System.out.println("Left");
+			
+			
+			
+		}else if(b.getKeyCode() == 39){
+//			System.out.println("Right");
+			
+			
+			
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		System.out.println("Key Released");
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		System.out.println("Key Typed");
 	}
-	
+
 	public KeyListener getKeyListener(){
 		return this;
 	}
--- GroupSymposium/src/joeyDaniel/Map.java
+++ GroupSymposium/src/joeyDaniel/Map.java
@@ -3,12 +3,14 @@ package joeyDaniel;
 import guiPractice.GUIApplication;
 import guiPractice.Screen;
 import guiPractice.components.Graphic;
+import rhbattlesandstuff.Character;
 
 public class Map extends GUIApplication{
 	
 	private static Floor3 screen3;
 	private static Floor1 screen1;
 	private static Map game;
+	public static Character x;
 	
 	public Map(){
 		super();
@@ -18,11 +20,14 @@ public class Map extends GUIApplication{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		x = new Character();
+		x.setLocation(3, 3);
 		game = new Map();
 		Thread app = new Thread(game);
 		app.start();
 		
 	}
+	
 	
 	
 	@Override
