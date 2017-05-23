98464e96c1c67e2323e461b88af31939c6f2231e
Student8 <Student8@10.8.33.167>
2017-05-23 13:56:09 -0400

    ?

--- GroupSymposium/src/joeyDaniel/Map.java
+++ GroupSymposium/src/joeyDaniel/Map.java
@@ -7,10 +7,15 @@ import rhbattlesandstuff.Character;
 
 public class Map extends GUIApplication{
 	
-	private static Floor3 screen3;
-	private static Floor1 screen1;
 	private static Map game;
 	public static Character x;
+	private static Floor1 screen1;
+	private static Floor2 screen2;
+	private static Floor3 screen3;
+	private static Floor4 screen4;
+	private static Floor5 screen5;
+	private static Floor6 screen6;
+	private static Floor7 screen7;
 	
 	public Map(){
 		super();
@@ -20,8 +25,6 @@ public class Map extends GUIApplication{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		x = new Character();
-		x.setLocation(3, 3);
 		game = new Map();
 		Thread app = new Thread(game);
 		app.start();
@@ -32,9 +35,14 @@ public class Map extends GUIApplication{
 	
 	@Override
 	protected void initScreen() {
-		screen3 = new Floor3();
 		screen1 = new Floor1();
-		setScreen(screen1);
+		screen2 = new Floor2();
+		screen3 = new Floor3();
+		screen4 = new Floor4();
+		screen5 = new Floor5();
+		screen6 = new Floor6();
+		screen7 = new Floor7();
+		setScreen(screen3);
 	}
 	
 	
