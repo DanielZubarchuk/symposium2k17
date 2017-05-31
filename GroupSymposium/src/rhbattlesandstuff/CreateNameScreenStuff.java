e449f80d01fb26b584ab4a5ba38039d9ff70b654
Student 8 <Student 8@10.8.33.171>
2017-05-31 14:02:25 -0400

    dada

--- GroupSymposium/src/rhbattlesandstuff/CreateNameScreenStuff.java
+++ GroupSymposium/src/rhbattlesandstuff/CreateNameScreenStuff.java
@@ -19,7 +19,7 @@ public class CreateNameScreenStuff extends FullFunctionScreen{
 	private TextLabel textPop;
 	private Graphic picPop;
 	private Button confirm; 
-//	private TextColoredLabel pop;
+	private TextColoredLabel pop;
 	
 	static Player realPlayer = new Character();
 
@@ -30,25 +30,33 @@ public class CreateNameScreenStuff extends FullFunctionScreen{
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-//		pop = new TextColoredLabel(100, 500, 500, 100, "Are you sure?", Color.BLUE, Color.BLACK);
+		pop = new TextColoredLabel(100, 500, 500, 100, "Are you sure?", Color.BLUE, Color.BLACK);
 		namestuff = new TextLabel(100,70,800,25, "Name goes here:");
 		typeName = new TextField(100, 100, 800, 48, "", "");
 		confirm = new Button(900, 550, 100, 50, "Confirm", Color.BLUE, new Action(){
+			
+			boolean shown = false;
 			public void act(){
 				
-//				viewObjects.add(pop);
-				
-				
-				if(typeName.getText().length()<=2 || typeName.getText().length()>=13){
-					System.out.println(typeName.getText());
-					typeName.setText("");
-					System.out.println("Please type in a name between 3-12 characters");
+				if(!shown){
+					addObject(pop);
+					shown = true;
 				}
 				else{
-					realPlayer.setName(typeName.getText());
-					NotPokemonDungeonFinalFantasyCrossOverGame.pmdffcog.setScreen(NotPokemonDungeonFinalFantasyCrossOverGame.gssScreen);
-					System.out.println(realPlayer.getName());;
+					remove(pop);
+					shown = false;
 				}
+				
+//				if(typeName.getText().length()<=2 || typeName.getText().length()>=13){
+//					System.out.println(typeName.getText());
+//					typeName.setText("");
+//					System.out.println("Please type in a name between 3-12 characters");
+//				}
+//				else{
+//					realPlayer.setName(typeName.getText());
+//					NotPokemonDungeonFinalFantasyCrossOverGame.pmdffcog.setScreen(NotPokemonDungeonFinalFantasyCrossOverGame.gssScreen);
+//					System.out.println(realPlayer.getName());;
+//				}
 			}
 		});
 		viewObjects.add(namestuff);
